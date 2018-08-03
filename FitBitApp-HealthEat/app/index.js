dfcecb5f9c4a3cdf4d4694d2729c385d9ddc8502
William Louis <bluepegasus123@users.noreply.github.com>
2018-08-02 18:17:23 -0700

    Updated code to improve UI
    Added additional code to make sure that the UI was more detailed and containing additional information about the foods scanned by the phone's app.

--- FitBitApp-HealthEat/app/index.js
+++ FitBitApp-HealthEat/app/index.js
@@ -14,17 +14,51 @@ clock.granularity = "minutes";
 
   var k = 1;
 
-const Color = document.getElementById("Color");
-const Score = document.getElementById("Score");
-const FoodName = document.getElementById("FoodName");
+const score = document.getElementById("score");
+const foodName = document.getElementById("foodName");
+const glutenFree = document.getElementById("glutenFree");
+const brainHealth = document.getElementById("brainHealth");
+const automaticDoNotEat = document.getElementById("automaticDoNotEat");
+const isCarcinogenic = document.getElementById("isCarcinogenic");
+
 
 function update(){
     //Reading JSON File Content
   let json_object  = fs.readFileSync("/mnt/assets/resources/Food_"+ k +".json", "json");
 
-  Color.text = json_object.Color;
-  Score.text = json_object.Score;
-  FoodName.text = json_object.FoodName;
+  score.text = json_object.score;
+  foodName.text = json_object.foodName;
+  
+   if(json_object.glutenFree === true) {
+  glutenFree.text = "Gluten Free: ✅";//"Gluten Free: " + json_object.glutenFree;
+  }
+   else {
+     glutenFree.text = "Gluten Free: ❌";
+   }
+  
+//   //Brain Healthy Conditional
+if(json_object.brainHealth === true) {
+   brainHealth.text = "Healthy for Brain: ✅";
+ }
+ else {
+  brainHealth.text = "Healthy for Brain: ❌";
+}
+  
+//   //Automatic Do Not Eat Food Conditional
+if(json_object.automaticDoNotEat === true) {
+   automaticDoNotEat.text = "Unhealthy Food: ✅";
+ }
+else {
+    automaticDoNotEat.text = "Unhealthy Food: ❌";
+ }
+  
+if(json_object.isCarcinogenic === true) {
+  isCarcinogenic.text = "Carcinogenic: ✅";
+}
+  else {
+    isCarcinogenic.text = "Carcinogenic: ❌";
+  }
+ 
    
 }
 
@@ -48,7 +82,7 @@ document.onkeypress = function(e) {
      console.log(k);
   }
   
-  if(e.key === "down" && k<3) {
+  if(e.key === "down" && k<4) {
     k++;
     console.log(k);
 
